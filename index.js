454614a2daf16451f4697005d6a046ecd405b335
stellamunson <sftamesis13@gmail.com>
2022-08-22 14:47:06 -0700

    commit tree 0e497dce0bfeed1c907e78d1b2f966e300a6f1d3

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+   cats.shift()
+}
+
+function appendCat(name){
+    let newCats = cats.slice();
+    newCats.push(name);
+    return newCats
+}
+
+function prependCat(name){
+    let newArray = cats.slice();
+    newArray.unshift(name);
+    return newArray;
+}
+
+function removeLastCat(){
+    const newArray = cats.slice();
+    newArray.pop();
+    return newArray;
+}
+
+function removeFirstCat(){
+    const newArray = cats.slice()
+    newArray.shift()
+    return newArray
+}
